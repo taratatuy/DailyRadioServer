2139350246e75ab1b27de78435e2f76af83965f2
Alexandr <taratatuy@yandex.ru>
2019-10-20 22:20:18 +0300

    Add musicplayer

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use('/auth', routes.authorization);
 app.use('/user', routes.user);
 app.use('/music', routes.music);
--- routes/music.js
+++ routes/music.js
@@ -1,21 +1,52 @@
 const express = require('express');
+const MM = require('music-metadata');
 const fs = require('fs');
-const filePath = './myMusic/music.mp3';
-const stat = fs.statSync(filePath);
+const util = require('util');
+
+// const filePath = './Music/';
+// const stat = fs.statSync(filePath);
 
 // const { SENTRY } = require('../config.js');
 // const database = require('../database');
 
+const readDirP = util.promisify(fs.readdir);
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  fs.createReadStream(filePath).pipe(res);
-  // console.log(stat);
+router.get('/playlist', async (req, res) => {
+  const files = await readDirP('./Music');
 
+  files.sort(() => Math.random() - 0.5);
+
+  res.json(files);
+});
+
+router.get('/play/:name', (req, res) => {
+  const name = req.params.name;
+  const filePath = './Music/' + name;
+
+  const stat = fs.statSync(filePath);
+  fs.createReadStream(filePath).pipe(res);
   res.writeHead(200, {
     'Content-Type': 'audio/mpeg',
     'Content-Length': stat.size
   });
+});
+
+router.get('/play/:name/stats', (req, res) => {
+  const name = req.params.name;
+  const filePath = './Music/' + name;
+
+  MM.parseFile(filePath)
+    .then(({ common }) => {
+      res.json({
+        title: common.title,
+        artist: common.artist,
+        picture: common.picture[0]
+      });
+    })
+    .catch(err => {
+      console.error(err.message);
+    });
 });
 
 // function getMusicStream() {}
