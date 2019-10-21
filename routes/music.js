01fef9d58dba70d9b1dff836a1fcdb0040074c78
Alexandr <taratatuy@yandex.ru>
2019-10-21 15:35:50 +0300

    Add exceptions processing

--- routes/music.js
+++ routes/music.js
@@ -2,32 +2,49 @@ const express = require('express');
 const MM = require('music-metadata');
 const fs = require('fs');
 const util = require('util');
+const { SENTRY } = require('../config.js');
 
 const readDirP = util.promisify(fs.readdir);
 const router = express.Router();
 
 router.get('/playlist', async (req, res) => {
-  const files = await readDirP('./Music');
+  try {
+    const files = await readDirP('./Music');
+    if (!files[0]) return res.status(503);
 
-  files.sort(() => Math.random() - 0.5);
-
-  res.json(files);
+    files.sort(() => Math.random() - 0.5);
+    res.json(files);
+  } catch (err) {
+    SENTRY.captureException('Get playlist processing error: ', err);
+    res.status(500).end('Internal Server Error');
+  }
 });
 
 router.get('/play/:name', (req, res) => {
-  const name = req.params.name;
-  const filePath = './Music/' + name;
+  try {
+    const name = req.params.name;
+    if (name === 'undefined')
+      return res.status(404).json({ message: 'Cant find track.' });
 
-  const stat = fs.statSync(filePath);
-  fs.createReadStream(filePath).pipe(res);
-  res.writeHead(200, {
-    'Content-Type': 'audio/mpeg',
-    'Content-Length': stat.size
-  });
+    const filePath = './Music/' + name;
+    const stat = fs.statSync(filePath);
+
+    fs.createReadStream(filePath).pipe(res);
+
+    res.writeHead(200, {
+      'Content-Type': 'audio/mpeg',
+      'Content-Length': stat.size
+    });
+  } catch (err) {
+    res.status(404).json({ message: 'Cant find track.' });
+  }
 });
 
 router.get('/play/:name/stats', (req, res) => {
   const name = req.params.name;
+  if (name === 'undefined')
+    return res.status(404).json({ message: 'Cant find track.' });
+
   const filePath = './Music/' + name;
 
   MM.parseFile(filePath)
@@ -39,7 +56,8 @@ router.get('/play/:name/stats', (req, res) => {
       });
     })
     .catch(err => {
-      console.error(err.message);
+      console.log(err);
+      res.status(404);
     });
 });
 
